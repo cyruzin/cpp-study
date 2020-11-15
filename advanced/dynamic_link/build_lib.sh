fc83c32e28c63f8037d0e94780a30ef15bfa455a
Cyro Dubeux <xorycx@gmail.com>
2020-11-15 10:18:16 -0300

    sh script improvements

--- advanced/dynamic_link/build_lib.sh
+++ advanced/dynamic_link/build_lib.sh
@@ -1,3 +1,8 @@
-g++ -shared -fPIC calc.cpp -o libcalc.so \
+#!/bin/sh
+
+# before run this script, type "source build.sh" 
+# to export the env var and then "./build.sh"
+
+g++ -shared -fpic calc.cpp -o libcalc.so \
 && g++ dynamic_link.cpp -lcalc -L. -o dynamic_link \
-&& mv libcalc.so ~/bin
+&& export LD_LIBRARY_PATH=.
